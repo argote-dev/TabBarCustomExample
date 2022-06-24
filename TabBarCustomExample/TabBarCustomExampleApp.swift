1141213863c717d6cfbca526da3184ed427597c5
David Argote <44904958+argote-dev@users.noreply.github.com>
2022-06-23 21:03:22 -0500

    Initial Commit

---
+++ TabBarCustomExample/TabBarCustomExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabBarCustomExampleApp.swift
+//  TabBarCustomExample
+//
+//  Created by David Argote on 23/06/22.
+//
+
+import SwiftUI
+
+@main
+struct TabBarCustomExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
